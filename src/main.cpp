355f054dab09a37bbc2aeb54cd078c545a4d701c
Pontus Liedgren <pop2strong4u@gmail.com>
2020-09-04 14:47:25 +0200

    Initial code

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,30 @@
 #include <Arduino.h>
 
+int green_led = 12;
+int red_led = 13;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(red_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int value = analogRead(A0);
+  float voltage = 5.0 * value / 1024;
+  Serial.println(voltage);
+  delay(1000);
+  
+  if (voltage < 1.5) {
+    digitalWrite(red_led, HIGH);
+    digitalWrite(green_led, LOW);
+  }
+  else {
+    digitalWrite(red_led, LOW);
+    digitalWrite(green_led, HIGH);
+  }
+
 }
